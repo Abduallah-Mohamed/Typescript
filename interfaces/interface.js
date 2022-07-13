d1409be28bb8e5ce22f0dfa6a04968fc41e687d8
Abduallah-Mohamed <Abduallahmohamed966@gmail.com>
2022-07-14 01:45:06 +0200

    learning new things on interfaces

--- interfaces/interface.js
+++ interfaces/interface.js
@@ -1,4 +1,9 @@
 "use strict";
 exports.__esModule = true;
+var obj = {
+    firstName: "John",
+    lastName: "hardy",
+    nickName: "mohsen"
+};
 var pt = { x: 1, y: 2 };
 console.log(pt);
--- interfaces/interface.ts
+++ interfaces/interface.ts
@@ -3,6 +3,27 @@ interface Point {
   y: number;
 }
 
+interface Person {
+  readonly id: number;
+  firstName: string;
+  lastName: string;
+  nickName?: string; // ? means optional
+  sayHi: () => string; // function signature
+  stayAwake(): string; // another function signature
+}
+
+const obj: Person = {
+  id: 1,
+  firstName: "John",
+  lastName: "hardy",
+  nickName: "mohsen",
+  sayHi: () => "hello",
+  stayAwake: () => "good morning",
+};
+
+obj.firstName = "farouq";
+// obj.id = 2; // I can not edit read only property
+
 const pt: Point = { x: 1, y: 2 };
 
 console.log(pt);
