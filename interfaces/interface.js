c3c212baff06898ea6633b6cc1731ce517ae1ade
Abduallah-Mohamed <Abduallahmohamed966@gmail.com>
2022-07-14 22:26:45 +0200

    play around with interfaces

--- interfaces/interface.js
+++ interfaces/interface.js
@@ -1,9 +1,51 @@
 "use strict";
 exports.__esModule = true;
 var obj = {
+    id: 1,
     firstName: "John",
     lastName: "hardy",
-    nickName: "mohsen"
+    nickName: "mohsen",
+    sayHi: function () { return "hello"; },
+    stayAwake: function () { return "good morning"; },
+    applyDiscount: function (discount) { return discount * 2; }
 };
+obj.firstName = "farouq";
+// obj.id = 2; // I can not edit read only property
 var pt = { x: 1, y: 2 };
 console.log(pt);
+var product = {
+    name: "apple",
+    price: 10,
+    getPriceWithTax: function (tax) { return tax * product.price; },
+    applyDiscount: function (discount) {
+        var newPrice = discount * product.price;
+        product.price = newPrice;
+        return product.price;
+    }
+};
+console.log(product.getPriceWithTax(0.1));
+console.log(product.applyDiscount(product.price));
+console.log(product.price);
+var dog = {
+    name: "mohsen",
+    age: 2,
+    breed: "poodle",
+    bark: function () { return "woof"; }
+};
+console.log(dog);
+console.log(dog.bark());
+var serviceDog = {
+    name: "mohsen",
+    age: 2,
+    breed: "poodle",
+    bark: function () { return "woof"; },
+    job: "Drug Sniffer"
+};
+console.log(serviceDog);
+var abduallah = {
+    name: "abduallah",
+    id: 1,
+    salary: 1000,
+    level: "junior",
+    language: ["typescript", "javascript"]
+};
--- interfaces/interface.ts
+++ interfaces/interface.ts
@@ -10,6 +10,7 @@ interface Person {
   nickName?: string; // ? means optional
   sayHi: () => string; // function signature
   stayAwake(): string; // another function signature
+  applyDiscount(discount: number): number;
 }
 
 const obj: Person = {
@@ -19,6 +20,7 @@ const obj: Person = {
   nickName: "mohsen",
   sayHi: () => "hello",
   stayAwake: () => "good morning",
+  applyDiscount: (discount: number) => discount * 2,
 };
 
 obj.firstName = "farouq";
@@ -27,5 +29,84 @@ obj.firstName = "farouq";
 const pt: Point = { x: 1, y: 2 };
 
 console.log(pt);
+
+interface Product {
+  name: string;
+  price: number;
+  getPriceWithTax(tax: number): number;
+  applyDiscount(discount: number): number;
+}
+
+const product: Product = {
+  name: "apple",
+  price: 10,
+  getPriceWithTax: (tax: number) => tax * product.price,
+  applyDiscount: (discount: number) => {
+    const newPrice = discount * product.price;
+    product.price = newPrice;
+    return product.price;
+  },
+};
+
+console.log(product.getPriceWithTax(0.1));
+console.log(product.applyDiscount(product.price));
+console.log(product.price);
+
+interface Dog {
+  name: string;
+  age: number;
+}
+
+interface Dog {
+  breed: string;
+  bark(): string;
+}
+
+const dog: Dog = {
+  name: "mohsen",
+  age: 2,
+  breed: "poodle",
+  bark: () => "woof",
+};
+
+interface ServiceDog extends Dog {
+  job: "Drug Sniffer" | "Dog Walker" | "bomb";
+}
+
+console.log(dog);
+console.log(dog.bark());
+
+const serviceDog: ServiceDog = {
+  name: "mohsen",
+  age: 2,
+  breed: "poodle",
+  bark: () => "woof",
+  job: "Drug Sniffer",
+};
+
+console.log(serviceDog);
+
+// multiple inheritance
+interface human {
+  name: string;
+}
+
+interface Employee {
+  readonly id: number;
+  salary: number;
+}
+
+interface Engineer extends human, Employee {
+  level: string;
+  language: string[];
+}
+
+const abduallah: Engineer = {
+  name: "abduallah",
+  id: 1,
+  salary: 1000,
+  level: "junior",
+  language: ["typescript", "javascript"],
+};
 
 export {};
